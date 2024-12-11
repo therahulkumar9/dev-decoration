cd573aa0cecf73e0b38b9302205a1331d1cc5ce2
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-12 02:52:49 +0530

    Spotlight effect added

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import Link from "next/link"
-import { Spotlight } from "@/components/ui/Spotlight"
+// import { Spotlight } from "@/components/ui/Spotlight"
 
 function HeroSection() {
   return (
     <div className="h-auto top-12 md:h-screen w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto px-4 py-16 md:py-0">
-      <Spotlight
+      {/* <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
-      />
+      /> */}
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-5xl md:text-8xl lg:text-9xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Patna Decoration
