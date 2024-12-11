466c226f1cbb3c456dd23deb605355af82c9ab37
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-12 02:52:49 +0530

    Spotlight effect added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import HeroSection from "@/components/HeroSection";
+import { Spotlight } from "@/components/ui/Spotlight";
 
 export default function Home() {
   return (
     <>
     <main className="min-h-screen bg-black/[0.96]">
       {/* <h1 className="text-3xl text-center">Patan Decoration</h1> */}
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
       <HeroSection />
     </main>
     </>
