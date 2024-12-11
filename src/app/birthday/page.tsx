7e22941d96a75981c74400dcd87c98861d23a38e
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-11 19:15:26 +0530

    Add Tailwind CSS styling to h1 tag

--- src/app/birthday/page.tsx
+++ src/app/birthday/page.tsx
@@ -1,6 +1,6 @@
 export default function Birthday(){
     return (
-        <h1>
+        <h1 className="text-3xl text-center text-pink-500 mt-8 bg-gray-800 p-4 rounded-lg">
             Birthday Decoration in Patna
         </h1>
     );
