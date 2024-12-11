78232bd82d469e665ed7cc2742ddf9e912142104
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-12 02:35:36 +0530

    Added HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <>
     <main className="min-h-screen bg-black/[0.96]">
       {/* <h1 className="text-3xl text-center">Patan Decoration</h1> */}
+      <HeroSection />
     </main>
     </>
   );
