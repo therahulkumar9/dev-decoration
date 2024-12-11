32c8772687af8d3aa8bba866a64145519df7fd99
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-11 19:46:19 +0530

    Added navigation item in RootLayout, created 'birthday' route layout, and structured 'baby' subfolder with page content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>Nav Item</nav>
         {children}
       </body>
     </html>
