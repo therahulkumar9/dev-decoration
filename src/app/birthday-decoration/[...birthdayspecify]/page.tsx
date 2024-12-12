0c16874175da7bf354f52bc9f3a4042692070277
Jyotish Kumar <kumarjyotish67303@gmail.com>
2024-12-12 10:23:06 +0530

    added functionality of card

---
+++ src/app/birthday-decoration/[...birthdayspecify]/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+import { usePathname } from "next/navigation"
+import data from "@/data/decoration_kits.json"
+import { useEffect, useState } from "react"
+import SpecificBirthdayCard from "@/components/SpecificBirthdayCard"
+
+interface DecorationPackage {
+  id: number;
+  title: string;
+  description: string;
+  price:number;
+  image:string;
+  
+
+
+}
+
+export default function BirthdaySpecification() {
+  const pathname = usePathname()
+  const ids = Number(pathname.slice(21)) 
+
+  const [filterdata, setFilterData] = useState<DecorationPackage[]>([])
+
+  useEffect(() => {
+    const filteredData = data.packages.filter((item) => item.id === ids)
+    setFilterData(filteredData)
+  }, [ids]) 
+
+  return (
+    <>
+      <div>
+        <h1>Birthday Specification</h1>
+        {filterdata.length > 0 ? (
+          filterdata.map((item) => (
+            <div key={item.id}>
+              <SpecificBirthdayCard title={item.title} price={item.price} image={item.image} id={item.id} description={item.description}/>
+            </div>
+          ))
+        ) : (
+          <p>No package found with this ID.</p>
+        )}
+      </div>
+    </>
+  )
+}
---
+++ src/app/birthday-decoration/page.tsx
@@ -0,0 +1,25 @@
+
+import birthdayData from "@/data/decoration_kits.json";
+import BirthdayCard from "@/components/BirthdayCard"; 
+import Link from "next/link";
+
+export default function BirthdayDecoration() {
+  return (
+    <>
+    <div className="p-20">
+      <div className="mt-4 sm:grid sm:grid-cols-3 gap-4 ">
+      
+        {birthdayData.packages.map((data, index) => (
+          <div key={index}>
+            <Link href={`/birthday-decoration/${data.id}`}>
+            
+            <BirthdayCard title={data.title} image={data.image} price={data.price} id={data.id}/>
+            </Link>
+           </div>
+         
+        ))}
+      </div>
+      </div>
+    </>
+  );
+}
