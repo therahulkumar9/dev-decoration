4588b1b923984d3959328923bdcb714a080631c2
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-12 15:40:56 +0530

    Added Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import { Spotlight } from "@/components/ui/Spotlight";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         fill="white"
       />
       <HeroSection />
+      <Footer />
     </main>
     </>
   );
