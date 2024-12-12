6711489fce1ee6ecb9d7e7abc6a12836f755b16b
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-12 12:00:41 +0530

    changes navbar-menu

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,12 +4,12 @@ import Link from "next/link"
 
 function HeroSection() {
   return (
-    <div className="h-auto top-12 md:h-screen w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto px-4 py-16 md:py-0">
+    <div className="h-auto md:h-screen w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto px-4 py-16 md:py-0">
       {/* <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       /> */}
-      <div className="p-4 relative z-10 w-full text-center">
+      <div className="p-4 top-28 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-5xl md:text-8xl lg:text-9xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Patna Decoration
         </h1>
