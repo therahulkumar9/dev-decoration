de0a5878b2a531a489406bb69934c0d069ee040b
jyotishpro <kumarjyotish67303@gmail.com>
2024-12-13 14:04:11 +0530

    added feature of slider

--- src/app/birthday-decoration/[...birthdayspecify]/page.tsx
+++ src/app/birthday-decoration/[...birthdayspecify]/page.tsx
@@ -1,31 +1,34 @@
-"use client"
-import { usePathname } from "next/navigation"
-import data from "@/data/birthday.json"
-import { useEffect, useState } from "react"
-import SpecificBirthdayCard from "@/components/SpecificBirthdayCard"
+"use client";
+import { usePathname } from "next/navigation";
+import data from "@/data/birthday.json";
+import { useEffect, useState } from "react";
+import SpecificBirthdayCard from "@/components/SpecificBirthdayCard";
 
 interface DecorationPackage {
   id: number;
   title: string;
+  slug: string;
   description: string;
-  price:number;
-  image:string;
-  inclusion:string;
-  
-
-
+  price: number;
+  originalPrice: number;
+  discount: number;
+  image: string;
+  image1: string;
+  image2: string;
+  image3: string;
+  inclusion: string;
 }
 
 export default function BirthdaySpecification() {
-  const pathname = usePathname()
-  const ids = Number(pathname.slice(21)) 
+  const pathname = usePathname();
+  const ids = Number(pathname.slice(21));
 
-  const [filterdata, setFilterData] = useState<DecorationPackage[]>([])
+  const [filterdata, setFilterData] = useState<DecorationPackage[]>([]);
 
   useEffect(() => {
-    const filteredData = data.packages.filter((item) => item.id === ids)
-    setFilterData(filteredData)
-  }, [ids]) 
+    const filteredData = data.packages.filter((item) => item.id === ids);
+    setFilterData(filteredData);
+  }, [ids]);
 
   return (
     <>
@@ -33,7 +36,16 @@ export default function BirthdaySpecification() {
         {filterdata.length > 0 ? (
           filterdata.map((item) => (
             <div key={item.id}>
-              <SpecificBirthdayCard inclusion={item.inclusion} title={item.title} price={item.price} image={item.image} id={item.id} description={item.description}/>
+              <SpecificBirthdayCard
+                inclusion={item.inclusion}
+                title={item.title}
+                price={item.price}
+                image1={item.image1}
+                image2={item.image2}
+                image3={item.image3}
+                id={item.id}
+                description={item.description}
+              />
             </div>
           ))
         ) : (
@@ -41,5 +53,5 @@ export default function BirthdaySpecification() {
         )}
       </div>
     </>
-  )
+  );
 }
