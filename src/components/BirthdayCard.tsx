aac407998a50ab9cabb0e60342ad1e14d83b663a
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-14 12:55:39 +0530

    Added images for bithday route

--- src/components/BirthdayCard.tsx
+++ src/components/BirthdayCard.tsx
@@ -10,11 +10,20 @@ interface BirthdayCardProps {
 export default function BirthdayCard({ title, image, price }: BirthdayCardProps) {
   return (
     <div className="mt-10">
-      <div className="border-2 mt-2 border-yellow-400 p-4">
-        <Image src={image} alt={title} width={300} height={200} />
-        <h1 className=" mt-2 font-semibold">{title}</h1>
-        <h2 className="text-green-500 text-md font-bold">₹ {price}</h2>
-      </div>
+  <div className="border-2 mt-2 border-yellow-400 p-4">
+    <div className="w-[300px] h-[200px] overflow-hidden">
+      <Image
+        src={image}
+        alt={title}
+        width={300}
+        height={200}
+        className="object-cover w-full h-full"
+      />
     </div>
+    <h1 className="mt-2 font-semibold">{title}</h1>
+    <h2 className="text-green-500 text-md font-bold">₹ {price}</h2>
+  </div>
+</div>
+
   );
 }
